b10abb88b130f2ec97e5ccdcf2e79703d85f7f55
biswajithhutech <biswajith@hutechsolutions.com>
2023-03-15 15:23:21 +0530

    first commit

---
+++ about.py
@@ -0,0 +1,42 @@
+import random
+highest = int(input("enter the highest Number: "))
+lowest = int(input("enter the lowest Number: "))
+answer = random.randint(lowest, highest)
+print(answer)
+print("please guess number between {} to {}".format(lowest, highest))
+guess = 0
+# while guess == 00:
+#     print("quit")
+#     break
+while guess != answer:
+    guess = int(input())
+    if guess == 0:
+        print("quit")
+        break
+    if guess == answer:
+        print("you guessed it right")
+    if guess < answer:
+        print("please guess higher")
+    else:
+        print("please guess lower")
+
+
+
+
+
+#     print('guess higher')
+#     guess=int(input())
+#     if guess == answer:
+#         print("you guessed it")
+#     else:
+#         print("you did not guess correctly")
+# elif guess>answer:
+#     print("guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("you guessed it")
+#     else:
+#         print("you did not guess correctly")
+# else:
+#     print("you got it")
+
